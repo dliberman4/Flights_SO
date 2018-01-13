74b43af323a14b4cae5c3123c5a4bb87e64ec63e
Unknown <dliberman@itba.edu.ar>
2018-01-13 17:43:03 -0300

    working with fork

---
+++ pruebas/concurrentserverprueba.c
@@ -0,0 +1,70 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int listener_socket;
+  int accepted_socket;
+  int address_size;
+  int reuse;
+  struct sockaddr_in address;
+  struct sockaddr_storage client;
+
+  if((listener_socket = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("error al crear el socket\n");
+  }
+
+  printf("listener_socket creado\n");
+
+  address.sin_family = PF_INET;
+  address.sin_port = htons(2000);
+
+  if(INADDR_ANY)
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  /*evitar problemas de reutilizacion del puerto*/
+  reuse = 1;
+  setsockopt(listener_socket, SOL_SOCKET, SO_REUSEADDR, (char *) reuse, sizeof(int));
+
+  if(bind(listener_socket, (struct sockaddr *) &address, sizeof(address)) == -1) {
+    printf("error en bind\n");
+  }
+  printf("bind OK!\n");
+
+  if(listen(listener_socket, 10) == -1) //10 es la maxima cantidad que puede atenderse
+    printf("Se alcanzó el máximo de clientes. Pruebe más tarde\n");
+
+  while(1) {
+    address_size = sizeof(client);
+    printf("esperando al cliente...\n");
+    accepted_socket = accept(listener_socket, (struct sockaddr *) &client, &address_size);
+    if(accepted_socket == -1) {
+      printf("error al aceptar\n");
+    }
+    switch(fork()) {
+      case 0:
+              close(listener_socket);
+              printf("soy el hijo\natendiendo al cliente\n");
+              send(accepted_socket, "Hola como estas? Me duermo...\n", 17, 0);
+              sleep(15);
+              send(accepted_socket, "Ya me dormi. Adios!\n", 17, 0);
+              close(accepted_socket);
+              exit(0);
+              break;
+      case -1:
+              printf("error en el fork\n");
+              close(accepted_socket);
+              break;
+      default:
+              printf("soy el padre, sigo\n");
+              close(accepted_socket);
+              break;
+    }
+  }
+  return 0;
+}
